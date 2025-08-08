f08caf6d2a8c11222e90aaf76812875619b6b082
e-losiev <lowk3y.loko@gmail.com>
2025-08-08 17:57:39 +0300

    BSC Deployed

--- scripts/deploy/1-LpInjector.ts
+++ scripts/deploy/1-LpInjector.ts
@@ -2,15 +2,15 @@ import { ethers, run } from "hardhat";
 import { LP_INJECTOR_BSC, OWNER } from "./constants";
 
 async function main() {
-    const factory = await ethers.getContractFactory("E280Injector");
-    const contract = await factory.deploy(OWNER);
-    await contract.waitForDeployment();
-    console.log("E280Injector deployed to: ", contract.target);
+    // const factory = await ethers.getContractFactory("E280Injector");
+    // const contract = await factory.deploy(OWNER);
+    // await contract.waitForDeployment();
+    // console.log("E280Injector deployed to: ", contract.target);
 
-    // await run("verify:verify", {
-    //     address: LP_INJECTOR_BSC,
-    //     constructorArguments: [OWNER],
-    // });
+    await run("verify:verify", {
+        address: LP_INJECTOR_BSC,
+        constructorArguments: [OWNER],
+    });
 }
 
 main().catch((error) => {
